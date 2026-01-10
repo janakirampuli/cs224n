f3156b1cd9bba6bab62a6f7a68e7f6efa0ce54c3
janakirampuli <janakirampuli@gmail.com>
2026-01-10 11:46:11 +0530

    added a2 code

--- a2/parser_model.py
+++ a2/parser_model.py
@@ -73,8 +73,17 @@ class ParserModel(nn.Module):
         ### 
         ### See the PDF for hints.
 
+        self.embed_to_hidden_weight = nn.Parameter(torch.rand((n_features * self.embed_size, hidden_size)))
+        self.embed_to_hidden_bias = nn.Parameter(torch.rand(hidden_size))
+        nn.init.xavier_uniform_(self.embed_to_hidden_weight)
+        nn.init.uniform_(self.embed_to_hidden_bias)
 
+        self.dropout = nn.Dropout(self.dropout_prob)
 
+        self.hidden_to_logits_weight = nn.Parameter(torch.rand((hidden_size, n_classes)))
+        self.hidden_to_logits_bias = nn.Parameter(torch.rand(n_classes))
+        nn.init.xavier_uniform_(self.hidden_to_logits_weight)
+        nn.init.uniform_(self.hidden_to_logits_bias)
 
         ### END YOUR CODE
 
@@ -107,8 +116,9 @@ class ParserModel(nn.Module):
         ###     View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
         ###     Flatten: https://pytorch.org/docs/stable/generated/torch.flatten.html
 
-
-
+        b, m = w.shape
+        x = torch.index_select(self.embeddings, dim=0, index=w.reshape(-1))
+        x = x.reshape(b, -1)
         ### END YOUR CODE
         return x
 
@@ -144,6 +154,10 @@ class ParserModel(nn.Module):
         ###     Matrix product: https://pytorch.org/docs/stable/torch.html#torch.matmul
         ###     ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
 
+        x = self.embedding_lookup(w)
+        h = F.relu(x @ self.embed_to_hidden_weight + self.embed_to_hidden_bias)
+        h = self.dropout(h)
+        logits = h @ self.hidden_to_logits_weight + self.hidden_to_logits_bias
 
         ### END YOUR CODE
         return logits
